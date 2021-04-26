e6e0fa41d9c283b32f925a97334e4b7da692e5cb
you know who <chorvickprocode@gmail.com>
2021-04-26 09:35:05 -0400

    set up server js and env var

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+const path = require("path");
 const PORT = 3000;
 
 const app = express();
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
